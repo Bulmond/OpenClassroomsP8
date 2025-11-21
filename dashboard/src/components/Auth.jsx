492b4236c8d20c4d547dc96aadce5249edca4651
Bulmond <lfilipe.mottaa@gmail.com>
2025-11-21 15:37:18 +0100

    testing timeout for page reload

--- dashboard/src/components/Auth.jsx
+++ dashboard/src/components/Auth.jsx
@@ -26,6 +26,9 @@ const Auth = ({ setAuth }) => {
             if (response.data) {
                 localStorage.setItem("token", response.data.token);
                 setAuth(user);
+                setTimeout(() => {
+                    window.location.reload();
+                }, 500);
                 navigate("/");
             }
         } catch (error) {
